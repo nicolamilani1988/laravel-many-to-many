023e7ab226fb2c5d569dc7119dc20ce39874ab70
nicolamilani1988 <nicola.milani1988@outlook.it>
2021-06-01 16:18:59 +0200

    scaffolding creato + migrations fatte

---
+++ Many2Many_Industry/resources/views/components/header.blade.php
@@ -0,0 +1,10 @@
+<div class="title">
+    <div class="logo">
+        <a href="{{route('home')}}">
+            <img src="{{asset('/storage/assets/images/logo.png')}}" alt="">
+        </a>
+    </div>
+    <div>
+        <h1>VINYL FACTORY</h1>
+    </div>
+</div>
