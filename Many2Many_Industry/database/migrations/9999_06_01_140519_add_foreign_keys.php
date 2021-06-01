56ed832a6a01e99b23795a19a3580684b368085f
nicolamilani1988 <nicola.milani1988@outlook.it>
2021-06-01 16:19:16 +0200

    ok migrations

---
+++ Many2Many_Industry/database/migrations/9999_06_01_140519_add_foreign_keys.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeys extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('employees', function (Blueprint $table) {
+
+            $table -> foreign('location_id', 'locationemployee')
+                   -> references('id')
+                   -> on('locations');
+        });
+
+        Schema::table('employee_task', function (Blueprint $table) {
+
+            $table -> foreign('employee_id', 'employeetask')
+                   -> references('id')
+                   -> on('employees');
+
+            $table -> foreign('task_id', 'taskemployee')
+                   -> references('id')
+                   -> on('tasks');       
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            $table -> dropForeign('locationemployee');
+        });
+
+        Schema::table('employee_task', function (Blueprint $table) {
+            $table -> dropForeign('employeetask');
+            $table -> dropForeign('taskemployee');
+        });
+    }
+}
